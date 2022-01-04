62e9598ea953329de940633bc1c4fb30936256cf
lamouchi.youssef <58522491+lamouchiyoussef@users.noreply.github.com>
2022-01-04 18:58:59 +0100

    first commit

---
+++ src/main/java/tn/isetsl/App.java
@@ -0,0 +1,14 @@
+package tn.isetsl;
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger logger = LoggerFactory.getLogger(App.class); 
+		logger.info("Hello world");
+    }
+}
